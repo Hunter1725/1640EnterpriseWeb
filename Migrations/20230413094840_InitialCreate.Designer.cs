7757c30e8ac29bc8b6ffc5b00758e57dce614305
Nguyen Ngoc Hai <hainngcc200162@fpt.edu.vn>
2023-04-13 17:33:02 +0700

    Deploy Azure

--- Migrations/20230413094840_InitialCreate.Designer.cs
+++ Migrations/20230413094840_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EnterpriseWeb.Migrations
 {
     [DbContext(typeof(EnterpriseWebIdentityDbContext))]
-    [Migration("20230413083449_InitialCreate")]
+    [Migration("20230413094840_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
